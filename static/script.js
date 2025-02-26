d1984be965c6ee27074b4a194ddf1d04dc4c5c6d
robinmonsere <monsere.robin@gmail.com>
2025-02-27 00:11:41 +0100

    feat: Updated breadcrumbs to use grey links and white current folder in VisionVault

--- static/script.js
+++ static/script.js
@@ -48,6 +48,8 @@ function fetchFiles(folderPath) {
                     if (index < breadcrumbs.length - 1) {
                         span.className = 'breadcrumb clickable';
                         span.addEventListener('click', () => fetchFiles(crumb.path));
+                    } else {
+                        span.className = 'breadcrumb current'; // Highlight current folder in white
                     }
                     return span;
                 });
